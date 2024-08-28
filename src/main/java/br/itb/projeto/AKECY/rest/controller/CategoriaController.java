478f14a511b6c0bf88d457e39481dd4ef43bd2a7
Erik Antero Santos Melo <rm86389@estudante.fieb.edu.br>
2024-08-28 07:58:50 -0300

    iniciando o repositorio no pc da escola

---
+++ src/main/java/br/itb/projeto/AKECY/rest/controller/CategoriaController.java
@@ -0,0 +1,32 @@
+package br.itb.projeto.AKECY.rest.controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.itb.projeto.AKECY.model.entity.Categoria;
+import br.itb.projeto.AKECY.service.CategoriaService;
+
+@RestController
+@RequestMapping("/categoria/")
+public class CategoriaController {
+
+	private CategoriaService categoriaService;
+
+	public CategoriaController(CategoriaService categoriaService) {
+		super();
+		this.categoriaService = categoriaService;
+	}
+
+	@GetMapping("findAll")
+	public ResponseEntity<List<Categoria>> findAll() {
+		List<Categoria> categorias = categoriaService.findAll();
+
+		return new ResponseEntity<List<Categoria>>(categorias, HttpStatus.OK);
+	}
+
+}
---
+++ src/main/java/br/itb/projeto/AKECY/service/CategoriaService.java
@@ -0,0 +1,31 @@
+package br.itb.projeto.AKECY.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import br.itb.projeto.AKECY.model.entity.Categoria;
+import br.itb.projeto.AKECY.model.repository.CategoriaRepository;
+import jakarta.transaction.Transactional;
+
+@Service
+public class CategoriaService {
+
+	private CategoriaRepository categoriaRepository;
+
+	public CategoriaService(CategoriaRepository categoriaRepository) {
+		super();
+		this.categoriaRepository = categoriaRepository;
+	}
+	
+	public List<Categoria> findAll(){
+		List<Categoria> categorias = categoriaRepository.findAll();
+		return categorias;
+	}
+	
+	@Transactional
+	public Categoria create(Categoria categoria) {
+		
+		return categoriaRepository.save(categoria);
+	}
+}
